294270ef45c57356d78e9737ea540adfe40dcff5
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-11-07 15:34:25 +0530

    Used angular Built in directives

--- src/app/components/about-interiart/about-interiart.component.ts
+++ src/app/components/about-interiart/about-interiart.component.ts
@@ -6,7 +6,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./about-interiart.component.scss']
 })
 export class AboutInteriartComponent implements OnInit {
-
+  information: any[] = [
+    {
+      'img': '/assets/interial.jpg',
+      'title': 'RECIDENCIAL DESIGN',
+      'about': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam dolor eum, obcaecati voluptas magnam voluptatem neque? Itaque minima excepturi dolores simi.'
+    },
+    {
+      'img': '/assets/interial.jpg',
+      'title':'COMMERCIAL DESIGN',
+      'about': 'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ipsam dolor eum, obcaecati voluptas magnam voluptatem neque? Itaque minima excepturi dolores simi.'
+    }
+  ];
   constructor() { }
 
   ngOnInit() {
