d7cbb17f18fd5af9c57fb3ebd1651360d445f4c0
Priyanka-pardeshi <priyankasspardeshi@gmail.com>
2021-11-07 14:50:37 +0530

    Update in style.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,12 @@ import { HomeComponent } from './components/home/home.component';
 import { QuoteComponent } from './components/quote/quote.component';
 import { AboutInteriartComponent } from './components/about-interiart/about-interiart.component';
 import { QuoteServiceComponent } from './components/quote-service/quote-service.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { FormsModule }   from '@angular/forms';
+import { TempComponent } from './components/temp/temp.component';
+
+
+
 
 @NgModule({
   declarations: [
@@ -14,11 +20,13 @@ import { QuoteServiceComponent } from './components/quote-service/quote-service.
     HomeComponent,
     QuoteComponent,
     AboutInteriartComponent,
-    QuoteServiceComponent
+    QuoteServiceComponent,
+    TempComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
